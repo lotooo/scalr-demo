8ad41458b70ff8d8b7f83ea8af087e697dc55d05
Matthieu Serrepuy <mserrepuy@cloudops.com>
2015-03-25 14:46:14 -0400

    Add read documents

--- matt.php
+++ matt.php
@@ -15,16 +15,26 @@ $collection = $db->mycol;
 
 echo "Collection selected succsessfully<br>";
 
-   $document = array( 
-      "title" => "MongoDB", 
-      "description" => "database", 
-      "likes" => 100,
-      "url" => "http://www.tutorialspoint.com/mongodb/",
-      "by", "tutorials point"
-   );
-   $collection->insert($document);
-   echo "Document inserted successfully<br>";
+$document = array( 
+        "title" => "MongoDB", 
+        "description" => "database", 
+        "likes" => rand(0,100),
+        "url" => "http://www.tutorialspoint.com/mongodb/",
+        "by", "tutorials point"
+);
+$collection->insert($document);
+echo "Document inserted successfully<br>";
 
+echo "<p>";
+
+$cursor = $collection->find();
+// iterate cursor to display title of documents
+foreach ($cursor as $document) {
+      echo $document["title"] . "<br>";
+      echo $document["description"] . "<br>";
+      echo $document["likes"] . "<br>";
+      echo $document["url"] . "<br>";
+      echo "<p>";
+}
 
 ?>
-
