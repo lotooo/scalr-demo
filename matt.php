28e0393ffcbb3fd188fd7976d1eca9d18ea49a1f
Matthieu Serrepuy <mserrepuy@cloudops.com>
2015-03-25 14:02:15 -0400

    add mongodb connection

--- matt.php
+++ matt.php
@@ -2,4 +2,9 @@
 
 echo "My php module works";
 
+$m = new MongoClient($server='int.mongo.e9a4134effe57c.scalr-dns.net'); // connect
+$m->authenticate($username='scalr', $password='Z9unmumHOt');
+
+
+
 ?>
