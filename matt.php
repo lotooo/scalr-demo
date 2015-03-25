b932bca5a35c9cf62731ca495a6af6e4593c0d5b
Matthieu Serrepuy <mserrepuy@cloudops.com>
2015-03-25 14:36:39 -0400

    working php

--- matt.php
+++ matt.php
@@ -1,10 +1,30 @@
 <?php
 
-echo "My php module works";
+echo "My php module works<br>";
 
-$m = new MongoClient($server='int.mongo.e9a4134effe57c.scalr-dns.net'); // connect
-$m->authenticate($username='scalr', $password='Z9unmumHOt');
+$m = new MongoClient($server='10.184.53.206', $options = array('username'=>'scalr', 'password' =>'Z9unmumHOt') ); // connect
 
+echo "Connection to database successfully<br>";
+
+//$m->authenticate($username='scalr', $password='Z9unmumHOt');
+$db = $m->matt;
+
+echo "Database matt selected<br>";
+$collection = $db->createCollection('mycol');
+$collection = $db->mycol;
+
+echo "Collection selected succsessfully<br>";
+
+   $document = array( 
+      "title" => "MongoDB", 
+      "description" => "database", 
+      "likes" => 100,
+      "url" => "http://www.tutorialspoint.com/mongodb/",
+      "by", "tutorials point"
+   );
+   $collection->insert($document);
+   echo "Document inserted successfully<br>";
 
 
 ?>
+
